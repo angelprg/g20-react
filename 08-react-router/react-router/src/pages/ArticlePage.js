b62ae14c9b918ecf1d0d7584a21231c024f2764d
Angel Resendiz <angel.resendiz@gmail.com>
2022-09-14 19:38:09 -0500

    cleanup function

--- 08-react-router/react-router/src/pages/ArticlePage.js
+++ 08-react-router/react-router/src/pages/ArticlePage.js
@@ -3,11 +3,22 @@ import { Link } from "react-router-dom";
 
 const ArticlePage = () => {
   const [locations, setLocations] = useState(null);
-
   useEffect(() => {
+    let mounted = true;
+    console.log('Montando ArticlePage')
     fetch("https://rickandmortyapi.com/api/location")
       .then((result) => result.json())
-      .then((data) => setLocations(data.results));
+      .then((data) => {
+        console.log('El fetch terminó')
+        if(mounted){
+          console.log('ejecutando setLocations')
+          setLocations(data.results)
+        }
+      });
+    return () => {
+      console.log('Desmontando ArticlePage')
+      mounted = false
+    }
   }, []);
 
   if (!locations) return <div>Cargando...</div>;
